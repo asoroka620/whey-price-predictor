d28a4d297a6037d314ca8cd126ad0e9574a06e01
Alex Soroka <66346804+asoroka620@users.noreply.github.com>
2026-09-10 11:30:32 -0700

    Transforms for US data, need to look into how the canada data looks

--- ETL.py
+++ ETL.py
@@ -18,4 +18,32 @@
 
 # The Keepa Extension Chart Hack (Easiest & Free)While Keepa charges for its API, their Keepa Browser Extension is 100% free and displays interactive timeline charts directly on Amazon.ca product pages.How to get the data: When you hover your mouse over the Keepa chart on Amazon, it tracks every single historical date and price.The Manual Export: Because you are only tracking 3 to 5 products for this project, you don't even need to write a scraper. You can open the Keepa panel on those few products, click on the "Data" or "Settings" tab within the extension widget, and look for an option to "Export product data". It allows you to download a completely free, highly detailed historical .csv file directly to your computer.
 
+import pandas as pd
+
+
+def extract():
+    us_dry_whey_prod = pd.read_csv("data/USDA Dry Whey Prod..csv")
+    canada_quarterly_stock = pd.read_csv("data/Stocks of specified dairy products, quarterly 1 2.csv")
+    canada_prod_concen_milk = pd.read_csv("data/Production of concentrated milk products.csv")
+    return us_dry_whey_prod, canada_quarterly_stock, canada_prod_concen_milk
+
+def transform_us_data(df):
+    #DELETE ROWS THAT CONTAIN YEAR, THIS PROVIDES THE YEARLY TOTAL
+    df = df[['YEAR','PERIOD','VALUE','COMMODITY']]
+    df = df[df['PERIOD'] != 'YEAR']
+    df = df.rename(columns = {"VALUE" : "POUNDS"})
+
+def transform_can_data(quarter, total):
+    #Want to combine these two datasets, focsu on cleaning up the quarterly one first
+    quarter = quarter[quarter['Commodity'] = "Whey powder"]
+    #Keeps only the whey powder row
+    #Want to pivot this table longer, get quarters in rows, whey produced in columns
+    quarter = pd.melt(
+        quarter,
+        value_vars = []
+    )
+
+def main():
+    us_dry_whey_prod, canada_quarterly_stock, canada_prod_concen_milk = extract()
+    transform_us_data(us_dry_whey_prod)
 
