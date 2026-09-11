9439e1717366154b5ed44298b835b8e0a8e12c15
Alex Soroka <66346804+asoroka620@users.noreply.github.com>
2026-09-11 09:21:37 -0700

    Finished ETL process for CSVs

--- ETL.py
+++ ETL.py
@@ -21,7 +21,7 @@
 import pandas as pd
 
 
-def extract():
+def extract_csv():
     us_dry_whey_prod = pd.read_csv("data/USDA Dry Whey Prod..csv")
     canada_quarterly_stock = pd.read_csv("data/Stocks of specified dairy products, quarterly 1 2.csv")
     canada_prod_concen_milk = pd.read_csv("data/Production of concentrated milk products.csv")
@@ -32,6 +32,7 @@ def transform_us_data(df):
     df = df[['YEAR','PERIOD','VALUE','COMMODITY']]
     df = df[df['PERIOD'] != 'YEAR']
     df = df.rename(columns = {"VALUE" : "POUNDS"})
+    return df
 
 def transform_can_data_quarter(quarter):
    # Maps the quarter totals to dictionairy
@@ -62,6 +63,7 @@ def transform_can_data_quarter(quarter):
                 "Year": year,
                 "Week": week,
                 "Tonnes": tonnes   })
+    return pd.DataFrame(df_list)
 
 def transform_can_data_total(total):
     # Create weeks column and assign a week number based on the date
@@ -69,9 +71,66 @@ def transform_can_data_total(total):
     # Pivot the table longer
     #Create a year and month column and split the year and month on " "
     #Create empty df, that contains year, week number, tonnes prod.
-    
+    whey = total[total["Commodity"] == "Whey powder"]
+
+    df_long = whey.melt(
+        id_vars = "Commodity",
+        var_name = "Date",
+        value_name = "Tonnes"
+    )
+    df_long["Tonnes"] = pd.to_numeric(df_long["Tonnes"])
+    df_long["Date"] = pd.to_datetime(df_long["Date"])
+    df_long["Year"] = df_long["Date"].dt.year
+    df_long["Month"] = df_long["Date"].dt.month_name()
+
+    month_week = {
+        "January" : range(1,6),
+        "February" : range(6,10),
+        "March" : range(10,14),
+        "April" : range(14,19),
+        "May" : range(19, 23),
+        "June" : range(23,27),
+        "July" : range(27,32),
+        "August" : range(32, 36),
+        "September" : range(36, 40),
+        "October" : range(40, 45),
+        "November" : range(45, 49),
+        "December" : range(49, 53)
+    }
+
+    df_weeks = []
+
+    for row in df_long.itertuples(index=False):
+
+        weeks = list(month_week[row.Month])
+        weekly_tonnes = row.Tonnes / len(weeks)
+
+        for week in weeks:
+            df_weeks.append({
+                "Year": row.Year,
+                "Week": week,
+                "Tonnes": weekly_tonnes
+            })
+
+    df_weeks = pd.DataFrame(df_weeks)
+    return df_weeks
+
+def merge_can_data(can_quarter, can_total):
+    combined = pd.concat([can_quarter,can_total], ignore_index = True)
+    combined = combined.sort_values(by=["Year", "Week"])
+    return combined
+
+def load(usa, can):
+    usa.to_csv('USA Whey Production.csv', index = False)
+    can.to_csv('Canada Whey Production.csv', index = False)
 
 def main():
-    us_dry_whey_prod, canada_quarterly_stock, canada_prod_concen_milk = extract()
-    transform_us_data(us_dry_whey_prod)
+    us_dry_whey_prod, canada_quarterly_stock, canada_prod_concen_milk = extract_csv()
+    us_dry_whey_prod = transform_us_data(us_dry_whey_prod)
+    canada_quarterly_stock = transform_can_data_quarter(canada_quarterly_stock)
+    canada_prod_concen_milk = transform_can_data_total(canada_prod_concen_milk)
+    canada_total_prod = merge_can_data(canada_quarterly_stock, canada_prod_concen_milk)
+
+    load(us_dry_whey_prod, canada_total_prod)
+
 
